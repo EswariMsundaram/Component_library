ffe3180b36abe34eb4d66933bf9df2fa4803c749
EswariMsundaram <eswarimsundaram@gmail.com>
2025-10-29 21:00:21 -0700

    Final commit

--- src/components/UserProfileCard.tsx
+++ src/components/UserProfileCard.tsx
@@ -10,27 +10,30 @@ export interface User {
 
 export interface UserProfileCardProps {
   user: User;
-  showEmail?: boolean | string;
-  showRole?: boolean | string;
+  showEmail?: boolean;
+  showRole?: boolean;
   onEdit?: (userId: string) => void;
   children?: React.ReactNode;
 }
 
 function UserProfileCard(props: UserProfileCardProps) {
+  
   const { user, showEmail, showRole, onEdit} = props; // extracting values from props
+  const userEmail=showEmail ? user.email : 'User E-mail not available';
+  const userRole= showRole ? user.role :'User Role not available';
 
   return (
-    <div className="grid grid-cols-2 gap-4 p-2 m-2 w-100 h-60 rounded-lg bg-white">
+    <div className="grid grid-cols-2 gap-4 p-2 m-2 w-100 h-50 rounded-lg bg-white">
       <div className="mt-2 h-20 w-30 rounded-full">
         <img src={user.avatarUrl} />
       </div>
-      <div className="flex flex-col justify-center pr-6">
-        <div className="text-lg text-center color-black m-2 font-bold p-2">
+      <div className="flex flex-col justify-center pr-4">
+        <div className="text-lg text-center color-black m-2 font-bold ">
           {user.name}
         </div>{" "}
         {/* fetching the user name from parent user */}
-        <div className="text-sm pb-2">{showEmail}</div>
-        <div className="text-sm pb-4">{showRole}</div>
+        <div className="text-sm pb-2">{userEmail}</div>
+        <div className="text-sm pb-2">{userRole}</div>
       </div>
       <div className="col-span-2 p-2  mb-4 rounded-lg bg-blue-500 md:items-center text-white 
             hover:text-gray-700 self-center w-90 ">
